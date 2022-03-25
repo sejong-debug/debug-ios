9a477a40dd8bb69b3c82ee87db3a323886d610d5
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-03-25 13:50:41 +0900

    initial commit

---
+++ Debug/Debug/DebugApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebugApp.swift
+//  Debug
+//
+//  Created by 이태현 on 2022/03/25.
+//
+
+import SwiftUI
+
+@main
+struct DebugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
