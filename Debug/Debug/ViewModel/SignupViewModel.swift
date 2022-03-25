3f52c82a162361e2e888a74bab75a30edda9ba05
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-03-25 17:41:29 +0900

    Create SignupViewModel.swift

---
+++ Debug/Debug/ViewModel/SignupViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  SignupViewModel.swift
+//  Debug
+//
+//  Created by 이태현 on 2022/03/25.
+//
+
+import Foundation
+
+class SignupViewModel: ObservableObject {
+    @Published var isSignup = false
+    
+    var username = ""
+    var password = ""
+    
+    func signup() {
+        
+        Webservice().signup(username: username, password: password) { result in
+            
+            switch result {
+            case .success(let message):
+                DispatchQueue.main.async {
+                    self.isSignup = true
+                }
+                print(message)
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.isSignup = true
+                } // 테스트용
+                print(error)
+            }
+        }
+    }
+}
