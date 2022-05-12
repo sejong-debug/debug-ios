7cd68d2f8ea2630eea800cf0fe43ece1ee4c6328
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-05-12 16:48:45 +0900

    사진 찍은 후 메모 작성 부분

--- Debug/Debug/View/createMemoView.swift
+++ Debug/Debug/View/createMemoView.swift
@@ -9,12 +9,78 @@ import SwiftUI
 
 struct createMemoView: View {
     @State var image: Image?
-    
+    @State var text: String = "메모를 작성해주세요"
+    @State var tapOn: Bool = false
+    @Environment(\.dismiss) var dismiss
+    @State var goToDetectView: Bool = false
+    @State var checkDetect = 0
     var body: some View {
         VStack {
-            image?.resizable().scaledToFit()
-            Text("전달받은 사진")
+            NavigationLink(isActive: $goToDetectView, destination: {
+                DetectView()
+            }, label: {})
+            HStack {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "arrow.backward")
+                        .foregroundColor(.black)
+                }
+                Spacer()
+            }
+            .padding(.vertical)
+            TextEditor(text: $text)
+                .foregroundColor(tapOn ? .black : .gray)
+                .background(
+                    Rectangle().stroke(lineWidth: 3)
+                        .foregroundColor(tapOn ? .black : .gray)
+                        .shadow(color: .black, radius: 5, x: 3, y: 3)
+                )
+                .onTapGesture {
+                    if !tapOn {
+                        text = ""
+                    }
+                    tapOn = true
+                }
+            
+            Spacer()
+            HStack {
+                Text("촬영 된\n이미지")
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 20, weight: .bold))
+                Spacer()
+                image?
+                    .resizable()
+                    .frame(width: 140, height: 140)
+            }
+            .padding(.horizontal, 40)
+            .padding(.vertical)
+            Button {
+                //이미지, 메모 서버에 전송을 하고 질병조회로 넘어가기
+                goToDetectView = true
+                checkDetect += 1
+            } label: {
+                HStack {
+                    Text("이미지, 메모 저장하기")
+                        .foregroundColor(.white)
+                        .font(.system(size: 25, weight: .bold))
+                        .padding(.horizontal)
+                        .padding(.vertical, 5)
+                }
+                .frame(maxWidth: .infinity)
+                .background(
+                    RoundedRectangle(cornerRadius: 18)
+                        .foregroundColor(.green)
+                )
+            }
         }
+        .onAppear {
+            if checkDetect != 0 {
+                dismiss()
+            }
+        }
+        .padding()
+        .navigationBarHidden(true)
     }
 }
 
