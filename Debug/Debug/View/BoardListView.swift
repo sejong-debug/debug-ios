9b3256a088e46f84c426346af1bc38722035db66
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-05-27 17:09:14 +0900

    board 목록 view

--- Debug/Debug/View/BoardListView.swift
+++ Debug/Debug/View/BoardListView.swift
@@ -12,14 +12,20 @@ struct BoardListView: View {
     @State var showingImagePopUp = false
     
     let image = Image(systemName: "plus.circle")
-//    let project: ProjectListResponse
-    let testArray: [String] = ["test1","test2","test3"]
     
     @State var createBoardIsActive = false
     @State var goToDetectView = false
     
     @State var isActive = false
     
+    @State var page = 0
+    
+    let spaceName = "scroll"
+
+    @State var wholeSize: CGSize = .zero
+    @State var scrollViewSize: CGSize = .zero
+    @State var projectID: Int = 0
+    @ObservedObject var boardListVM = BoardListViewModel()
     var body: some View {
         NavigationView {
             ZStack {
@@ -31,43 +37,80 @@ struct BoardListView: View {
                             Image(systemName: "arrow.backward")
                                 .foregroundColor(.black)
                         }
-
                         Spacer()
-//                        Text("작물 목록")
-//                            .font(.system(size: 24, weight: .bold))
-//                        Spacer()
-//                        Image(systemName: "arrow.backward")
-//                            .foregroundColor(.black)
-//                            .opacity(0)
+                        Text("작물조회")
+                        Spacer()
+                        
                     }
                     .padding([.leading,.trailing,.bottom])
-                    if !testArray.isEmpty {
-                        List {
-                            ForEach(testArray, id: \.self) { test in
-                                
-                                NavigationLink(destination: {
-                                    DetectView()
-                                }, label: {
-                                    HStack {
-                                        Image(systemName: "circle.fill")
-                                            .resizable()
-                                            .frame(width: 70, height: 70)
-                                        Text(test)
-                                            .padding(.horizontal)
+                    if !boardListVM.boardListData.isEmpty {
+                        ChildSizeReader(size: $wholeSize) {
+                            ScrollView {
+                                ChildSizeReader(size: $scrollViewSize) {
+                                    VStack(spacing: 20) {
+                                        ForEach(boardListVM.boardListData.flatMap{ $0 }, id: \.self) { board in
+                                            NavigationLink(destination: {
+                                                DetectView(projectID: projectID, boardID: board.boardID)
+                                            }, label: {
+                                                HStack {
+                                                    AsyncImage(url: URL(string: board.boardImageURI)) { image in
+                                                        image
+                                                            .resizable()
+                                                            .scaledToFit()
+                                                    } placeholder: {
+                                                        ProgressView()
+                                                    }
+                                                    .frame(width: 70, height: 70) //이미지 처리 부분
+                                                    Spacer()
+                                                    Text(board.memo)
+                                                        .padding(.horizontal)
+                                                }
+                                                .background(
+                                                    RoundedRectangle(cornerRadius: 15)
+                                                        .stroke(lineWidth: 2)
+                                                        .foregroundColor(.green)
+                                                )
+                                            })
+                                        }
                                     }
-                                })
-//                                NavigationLink(destination: DetectView(rootIsActive: $isActive), isActive: $isActive, label: {
-//                                        HStack {
-//                                            Image(systemName: "circle.fill")
-//                                                .resizable()
-//                                                .frame(width: 70, height: 70)
-//                                            Text(test)
-//                                                .padding(.horizontal)
-//                                        }
-//                                })
+                                    .listStyle(PlainListStyle())
+                                    .background(
+                                        GeometryReader { proxy in
+                                            Color.clear.preference(
+                                                key: ViewOffsetKey.self,
+                                                value: -1 * proxy.frame(in: .named(spaceName)).origin.y
+                                            )
+                                        }
+                                    )
+                                    .onPreferenceChange(
+                                        ViewOffsetKey.self,
+                                        perform: { value in
+                                            print("offset: \(value)") // offset: 1270.3333333333333 when User has reached the bottom
+                                            print("height: \(scrollViewSize.height)") // height: 2033.3333333333333
+
+                                            if value >= scrollViewSize.height - wholeSize.height {
+                                                if boardListVM.boardListData.flatMap({ $0 }).count == (page+1)*10 {
+                                                    boardListVM.loadBoardList(projectID: projectID, page: page)
+                                                    page += 1
+                                                }
+                                                print("User has reached the bottom of the ScrollView.")
+                                                print(page)
+                                            } else {
+                                                print("not reached.")
+                                            }
+                                        }
+                                    )
+                                }
                             }
+                            .coordinateSpace(name: spaceName)
                         }
-                        .listStyle(PlainListStyle())
+                        .onChange(
+                            of: scrollViewSize,
+                            perform: { value in
+                                print(value)
+                            }
+                        )
+                        .padding(.horizontal)
                     } else {
                         Spacer()
                         Text("작물이 존재하지 않습니다.")
@@ -91,11 +134,16 @@ struct BoardListView: View {
                     .padding(.trailing)
                     .frame(height:40)
                 }
-                ImagePopUpView(showing: $showingImagePopUp)
+                ImagePopUpView(showing: $showingImagePopUp, projectID: projectID)
             }//작물목록 title 다시 만들기
             .navigationBarHidden(true)
         }
         .navigationBarHidden(true)
+        .onAppear {
+            if page == 0 {
+                boardListVM.loadBoardList(projectID: projectID, page: page)
+            }
+        }
     }
 }
 
