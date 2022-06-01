6bcad60a6692e4a8273d1baa633030cdd44ee98d
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-06-01 15:18:00 +0900

    질병의 수 받아오기

--- Debug/Debug/Model/ProjectListModel.swift
+++ Debug/Debug/Model/ProjectListModel.swift
@@ -52,49 +52,7 @@ struct ProjectListResponse: Codable {
     }
 }
 
-// MARK: - DataClass
-
-
-
-//// MARK: - Welcome
-//struct ProjectListResponse: Codable {
-//    let success: Bool
-//    let data: DataClass
-//
-//    struct DataClass: Codable {
-//        let content: [Content]
-//        let pageable: Pageable
-//        let numberOfElements, number: Int
-//        let sort: Sort
-//        let first, last: Bool
-//        let size: Int
-//        let empty: Bool
-//    }
-//
-//    // MARK: - Content
-//    struct Content: Codable, Hashable {
-//        let projectID: Int
-//        let name, cropType, startDate, endDate: String
-//        let completed: Bool
-//
-//        enum CodingKeys: String, CodingKey {
-//            case projectID = "projectId"
-//            case name, cropType, startDate, endDate, completed
-//        }
-//    }
-//
-//    // MARK: - Pageable
-//    struct Pageable: Codable {
-//        let sort: Sort
-//        let pageNumber, pageSize, offset: Int
-//        let paged, unpaged: Bool
-//    }
-//
-//    // MARK: - Sort
-//    struct Sort: Codable {
-//        let sorted, unsorted, empty: Bool
-//    }
-//
-//}
-//
-//// MARK: - DataClass
+struct diseaseResponse: Codable {
+    let success: Bool
+    let data: Int
+}
